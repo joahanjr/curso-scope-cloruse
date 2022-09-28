cbef7e3cbc617f8cef2b66a61136bcc9b99122ce
joahan jimenez <joahanramirez7@gmail.com>
2022-09-27 23:19:18 -0500

    use strict

---
+++ src/scope/strict.js
@@ -0,0 +1,14 @@
+"use strict"
+pi = 3.1416;
+
+console.log(pi);
+
+function myFuncion(){
+    "use strict"
+    return pi = 3.1416;
+}
+
+console.log(myFuncion());
+
+// "use strict"
+/* al poner eso y ejecutarlo lo que nos dice es que no podemos imprimir esa variable porque no esta declarada */
