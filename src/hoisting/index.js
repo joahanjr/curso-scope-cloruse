023f43ff470a3cfc67a539cb29869135401e9080
joahan jimenez <joahanramirez7@gmail.com>
2022-09-28 00:06:00 -0500

    hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,11 @@
+//var nameOfDog = undefined ; así guarda una variable cuando la usamos antes de ser declarada
+console.log(nameOfDog);
+var nameOfDog = "max";
+console.log(nameOfDog);
+
+
+nameOfDog();
+function nameOfDog(){
+    console.log("el mejor perro del mundo es: " + herName);
+}
+var herName = "max";
